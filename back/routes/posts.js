43aff6463339a1edb0e35b78728d57fb7069d8e6
taeminahn <ahntaemin@naver.com>
2022-12-08 17:30:37 +0900

    git 계정변경 테스트

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res, next) => { // GET /posts
   try{
     const posts = await Post.findAll({
       limit: 10,
-      offset: 10, // 11~20
+      offset: 10, // 11~20dd
     });
     res.status(200).json(posts);
   }catch (error){
