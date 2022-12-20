d913b5a07e1b739f46979dd94c557e24cc345e9f
taeminahn <ahntaemin@naver.com>
2022-12-20 17:53:11 +0900

    about page remove, backUrl add

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -3,6 +3,7 @@ import {Button, Form, Input} from 'antd';
 import {useDispatch, useSelector} from 'react-redux';
 import {ADD_POST_REQUEST, REMOVE_IMAGE, UPLOAD_IMAGES_REQUEST} from '../reducers/post';
 import useInput from '../hooks/useInput';
+import {backUrl} from "../config/config";
 
 const PostForm = () => {
   const dispatch = useDispatch();
@@ -74,7 +75,7 @@ const PostForm = () => {
       <div>
         {imagePaths.map((v, i) => (
           <div key={v} style={{display: 'inline-block'}}>
-            <img src={`http://localhost:3065/${v}`} style={{width: '200px'}} alt={v} />
+            <img src={`${backUrl}/${v}`} style={{width: '200px'}} alt={v} />
             <div>
               <Button onClick={onRemoveImage(i)}>제거</Button>
             </div>
