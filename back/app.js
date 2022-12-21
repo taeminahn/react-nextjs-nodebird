5db3384075dc2cf91e83b617027e0e12f95d91a1
taeminahn <ahntaemin@naver.com>
2022-12-21 13:40:40 +0900

    fixed cors(2)

--- back/app.js
+++ back/app.js
@@ -37,7 +37,7 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 app.use(cors({
-  origin: ['http://localhost:3060', 'nodebird.com', 'http://3.34.177.67'],
+  origin: ['http://localhost:3060', 'nodebird.com', 'http://43.201.96.208'],
   credentials: true,
 }));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
