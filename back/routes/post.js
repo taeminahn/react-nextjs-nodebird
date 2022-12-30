7d97bb6e5baf33140749c50e9e59156751089054
taeminahn <ahntaemin@naver.com>
2022-12-30 14:04:53 +0900

    오타 수정

--- back/routes/post.js
+++ back/routes/post.js
@@ -20,11 +20,11 @@ try {
 AWS.config.update({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  region: 'ap-northeast-2',
+  region: 'ap-northeast-2'
 });
 
 const upload = multer({
-  storage: multer.diskStorage({
+  storage: multerS3({
     s3: new AWS.S3(),
     bucket: 'react-incutepig-s3',
     key(req, file, cb) {
