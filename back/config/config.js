13976fcb6a9c2b6061bb5f7902d3c093053176aa
taeminahn <ahntaemin@naver.com>
2022-12-30 11:04:44 +0900

    config edit

--- back/config/config.js
+++ back/config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 module.exports = {
   'development': {
     'username': 'root',
-    'password': process.env.DB_PASSWORD,
+    'password': process.env.DB_PASSWORD_DEV,
     'database': 'react_nodebird',
     'host': '127.0.0.1',
     'dialect': 'mysql'
