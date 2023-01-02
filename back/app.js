d19149bb98d6c70869e5b9df2adf36e65c080579
taeminahn <ahntaemin@naver.com>
2023-01-02 12:17:56 +0900

    http -> https 적용 관련 수정2

--- back/app.js
+++ back/app.js
@@ -54,7 +54,7 @@ app.use(session({
   secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
-    secure: false,
+    secure: true,
     domain: process.env.NODE_ENV === 'production' && '.incutepig.site'
   }
 }));
